5f92ca72526b391710ca0c8294ded2d966143f63
Sathish Infiniti <64281230+sathish-infiniti@users.noreply.github.com>
2020-04-30 22:06:37 +0530

    Update InfinitiBomb.sh

--- InfinitiBomb.sh
+++ InfinitiBomb.sh
@@ -4,9 +4,9 @@ figlet -f block Infiniti
 echo -e "\e[1;34m Presents \e[0m"
 echo -e "\e[1;32m"
 figlet InfinitiBomb
-echo -e "\e\n[1;31m-            Note : Please Dont MisUse the Script          -\e[0m\n"
-echo "-                  Continue At Your Own Risk               -"
-echo -e "\e\n[1;32m-             ----  Press Enter To Continue  ----          -\e[0m"
+echo -e "\e\n[1;31m-           Note : Please Dont MisUse the Script          -\e[0m\n"
+echo "-                 Continue At Your Own Risk               -"
+echo -e "\e\n[1;32m-            ----  Press Enter To Continue  ----          -\e[0m"
        #https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ  
 echo " "
 read a1
@@ -30,18 +30,18 @@ rm *.xxx >/dev/null 2>&1
 clear
 echo -e "\e[1;31m"
 figlet InfinitiBomb
-echo -e "\e[1;32m Created By \e[1;34m"
+echo -e "\n\e[1;32m Created By \e[1;34m\n"
 figlet -f shadow Sathish
 figlet -f shadow Infiniti
-echo -e "\e[1;32m           This InfinitiBomber Was Created By Infiniti \e[0m"
-echo -e "\e[1;32m         For Help and other Queries Mail Me at below id\e[0m"
-echo -e "\e[1;34m              Mail: sathishinfinitihub@gmail.com \e[0m"
-echo -e "\e[1;34m                         YouTube Page \e[0m"
-echo -e "\e[4;34m -https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
+echo -e "\n\e[1;32m          This InfinitiBomber Was Created By Infiniti \e[0m"
+echo -e "\e[1;32m        For Help and other Queries Mail Me at below id\e[0m"
+echo -e "\e[1;34m             Mail: sathishinfinitihub@gmail.com \e[0m"
+echo -e "\e[1;34m                        YouTube Page \e[0m"
+echo -e "\e[4;34m-https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
 
 
 echo " "
-echo -e "\e[1;31m              Don't do Stupid Things Using This!!! \e[0m"
+echo -e "\e[1;31m             Don't do Stupid Things Using This!!! \e[0m"
 echo " "
 echo "Enter 1   =>    SMS Bombing "
 echo "Enter 2   =>    CALL Bombing "
@@ -90,11 +90,11 @@ elif [ $ch -eq 4 ];then
 clear
 echo -e "\e[1;33m"
 figlet InfinitiBomb
-echo -e "\e[1;34mCreated By \e[1;34m"
+echo -e "\n\e[1;34mCreated By \e[1;34m\n"
 figlet Sathish
 figlet Infiniti 
 echo  " "
-echo -e "\e[1;32m                   Features\e[1;34m"
+echo -e "\n\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
 echo "  [+] International Bombing"
 echo "  [+] Call Bombing "
@@ -103,16 +103,16 @@ echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo ""
 echo ""
-echo -e "\e[1;31m   This Script is Only For Educational Purposes or To Prank.\e[0m"
-echo -e "\e[1;31m              Do not Use This To Harm Others. \e[0m"
-echo -e "\e[1;31m      I Am Not Responsible For The Misuse Of The Script. \e[0m"
-echo -e "\e[1;32m         Make Sure To Update it If It Does not Work.\e[0m"
+echo -e "\n\e[1;31m  This Script is Only For Educational Purposes or To Prank.\e[0m"
+echo -e "\e[1;31m             Do not Use This To Harm Others. \e[0m"
+echo -e "\e[1;31m     I Am Not Responsible For The Misuse Of The Script. \e[0m"
+echo -e "\e[1;32m        Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
-echo -e "\e[1;31m                        That's All !!!\e[0m"
-echo -e "\e[1;32m         For Help and other Queries Mail Me at below id\e[0m"
-echo -e "\e[1;34m              Mail: sathishinfinitihub@gmail.com \e[0m"
-echo -e "\e[1;34m                         YouTube Page \e[0m"
-echo -e "\e[4;34m -https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
+echo -e "\n\e[1;31m                       That's All !!!\e[0m"
+echo -e "\e[1;32m        For Help and other Queries Mail Me at below id\e[0m"
+echo -e "\e[1;34m             Mail: sathishinfinitihub@gmail.com \e[0m"
+echo -e "\e[1;34m                        YouTube Page \e[0m"
+echo -e "\e[4;34m-https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
 
 echo "Press Enter To Go Home"
 read a3
@@ -121,17 +121,17 @@ elif [ $ch -eq 5 ];then
 clear
 echo -e "\e[1;31m"
 figlet InfinitiBomb
-echo -e "\e[1;34m Created By \e[1;32m"
+echo -e "\n\e[1;34m Created By \e[1;32m\n"
 figlet Sathish
 figlet Infiniti 
-echo -e "\e[1;32m         For Help and other Queries Mail Me at below id\e[0m"
-echo -e "\e[1;34m              Mail: sathishinfinitihub@gmail.com \e[0m"
-echo -e "\e[1;34m                         YouTube Page \e[0m"
-echo -e "\e[4;34m -https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
+echo -e "\n\e[1;32m        For Help and other Queries Mail Me at below id\e[0m"
+echo -e "\e[1;34m             Mail: sathishinfinitihub@gmail.com \e[0m"
+echo -e "\e[1;34m                        YouTube Page \e[0m"
+echo -e "\e[4;34m-https://www.youtube.com/channel/UCjFLAHa6fDRqrhJROgGRTZQ-\e[0m"
 echo " "
 exit 0
 else
-echo -e "\e[4;32m Invalid Input !!! \e[0m"
+echo -e "\n\e[4;32m Invalid Input !!! \e[0m\n"
 echo "Press Enter To Go Home"
 read a3
 clear
